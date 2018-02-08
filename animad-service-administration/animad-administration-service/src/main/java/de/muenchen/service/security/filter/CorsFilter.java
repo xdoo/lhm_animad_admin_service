3502b5edfbf7367a96760c8b2ec09c6b41fe7de2
rowe42 <rolandwrn@gmail.com>
2018-02-08 13:06:46 +0100

    Zusätzliche Header zum CorsFilter ergänzt für CORS mit Credentials

--- animad-service-administration/animad-administration-service/src/main/java/de/muenchen/service/security/filter/CorsFilter.java
+++ animad-service-administration/animad-administration-service/src/main/java/de/muenchen/service/security/filter/CorsFilter.java
@@ -19,7 +19,8 @@ public class CorsFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token, x-requested-with");
